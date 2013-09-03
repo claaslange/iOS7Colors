2e41ca25555084993df2d32e39fdb7f8bf3bbd5f
JP Simard <jp@jpsim.com>
2013-09-03 13:34:01 -0400

    Added all gradient colors from http://dribbble.com/shots/1186156-iOS-7-Color-Swatches

--- iOS7Colors/UIColor+iOS7Colors.h
+++ iOS7Colors/UIColor+iOS7Colors.h
@@ -10,6 +10,7 @@
 
 @interface UIColor (iOS7Colors)
 
+// Plain Colors
 + (instancetype)iOS7redColor;
 + (instancetype)iOS7orangeColor;
 + (instancetype)iOS7yellowColor;
@@ -21,6 +22,35 @@
 + (instancetype)iOS7darkGrayColor;
 + (instancetype)iOS7lightGrayColor;
 
+// Gradient Colors
++ (instancetype)iOS7redGradientBottomColor;
++ (instancetype)iOS7redGradientTopColor;
 
++ (instancetype)iOS7orangeGradientBottomColor;
++ (instancetype)iOS7orangeGradientTopColor;
+
++ (instancetype)iOS7yellowGradientBottomColor;
++ (instancetype)iOS7yellowGradientTopColor;
+
++ (instancetype)iOS7greenGradientBottomColor;
++ (instancetype)iOS7greenGradientTopColor;
+
++ (instancetype)iOS7tealGradientBottomColor;
++ (instancetype)iOS7tealGradientTopColor;
+
++ (instancetype)iOS7blueGradientBottomColor;
++ (instancetype)iOS7blueGradientTopColor;
+
++ (instancetype)iOS7violetGradientBottomColor;
++ (instancetype)iOS7violetGradientTopColor;
+
++ (instancetype)iOS7magentaGradientBottomColor;
++ (instancetype)iOS7magentaGradientTopColor;
+
++ (instancetype)iOS7blackGradientBottomColor;
++ (instancetype)iOS7blackGradientTopColor;
+
++ (instancetype)iOS7silverGradientBottomColor;
++ (instancetype)iOS7silverGradientTopColor;
 
 @end
--- iOS7Colors/UIColor+iOS7Colors.m
+++ iOS7Colors/UIColor+iOS7Colors.m
@@ -10,14 +10,18 @@
 
 @implementation UIColor (iOS7Colors)
 
+#pragma mark - Plain Colors
+
 + (instancetype)iOS7redColor;
 {
     return [UIColor colorWithRed:1.0f green:0.22f blue:0.22f alpha:1.0f];
 }
+
 + (instancetype)iOS7orangeColor;
 {
     return [UIColor colorWithRed:1.0f green:0.58f blue:0.21f alpha:1.0f];
 }
+
 + (instancetype)iOS7yellowColor;
 {
     return [UIColor colorWithRed:1.0f green:0.79f blue:0.28f alpha:1.0f];
@@ -58,5 +62,106 @@
     return [UIColor colorWithRed:0.78f green:0.78f blue:0.8f alpha:1.0f];
 }
 
+#pragma mark - Gradient Colors
+
++ (instancetype)iOS7redGradientBottomColor;
+{
+    return [UIColor colorWithRed:1.0f green:0.16f blue:0.41f alpha:1.0f];
+}
+
++ (instancetype)iOS7redGradientTopColor;
+{
+    return [UIColor colorWithRed:1.0f green:0.37f blue:0.23f alpha:1.0f];
+}
+
++ (instancetype)iOS7orangeGradientBottomColor;
+{
+    return [UIColor colorWithRed:1.0f green:0.37f blue:0.23f alpha:1.0f];
+}
+
++ (instancetype)iOS7orangeGradientTopColor;
+{
+    return [UIColor colorWithRed:1.0f green:0.58f blue:0.0f alpha:1.0f];
+}
+
++ (instancetype)iOS7yellowGradientBottomColor;
+{
+    return [UIColor colorWithRed:1.0f green:0.8f blue:0.01f alpha:1.0f];
+}
+
++ (instancetype)iOS7yellowGradientTopColor;
+{
+    return [UIColor colorWithRed:1.0f green:0.86f blue:0.3f alpha:1.0f];
+}
+
++ (instancetype)iOS7greenGradientBottomColor;
+{
+    return [UIColor colorWithRed:0.04f green:0.83f blue:0.09f alpha:1.0f];
+}
+
++ (instancetype)iOS7greenGradientTopColor;
+{
+    return [UIColor colorWithRed:0.53f green:0.99f blue:0.44f alpha:1.0f];
+}
+
++ (instancetype)iOS7tealGradientBottomColor;
+{
+    return [UIColor colorWithRed:0.35f green:0.78f blue:0.98f alpha:1.0f];
+}
+
++ (instancetype)iOS7tealGradientTopColor;
+{
+    return [UIColor colorWithRed:0.32f green:0.93f blue:0.78f alpha:1.0f];
+}
+
++ (instancetype)iOS7blueGradientBottomColor;
+{
+    return [UIColor colorWithRed:0.11f green:0.38f blue:0.94f alpha:1.0f];
+}
+
++ (instancetype)iOS7blueGradientTopColor;
+{
+    return [UIColor colorWithRed:0.10f green:0.84f blue:0.99f alpha:1.0f];
+}
+
++ (instancetype)iOS7violetGradientBottomColor;
+{
+    return [UIColor colorWithRed:0.35f green:0.34f blue:0.84f alpha:1.0f];
+}
+
++ (instancetype)iOS7violetGradientTopColor;
+{
+    return [UIColor colorWithRed:0.78f green:0.27f blue:0.99f alpha:1.0f];
+}
+
++ (instancetype)iOS7magentaGradientBottomColor;
+{
+    return [UIColor colorWithRed:0.78f green:0.26f blue:0.99f alpha:1.0f];
+}
+
++ (instancetype)iOS7magentaGradientTopColor;
+{
+    return [UIColor colorWithRed:0.94f green:0.30f blue:0.71f alpha:1.0f];
+}
+
++ (instancetype)iOS7blackGradientBottomColor;
+{
+    return [UIColor colorWithRed:0.17f green:0.17f blue:0.17f alpha:1.0f];
+}
+
++ (instancetype)iOS7blackGradientTopColor;
+{
+    return [UIColor colorWithRed:0.29f green:0.29f blue:0.29f alpha:1.0f];
+}
+
++ (instancetype)iOS7silverGradientBottomColor;
+{
+    return [UIColor colorWithRed:0.54f green:0.55f blue:0.56f alpha:1.0f];
+}
+
++ (instancetype)iOS7silverGradientTopColor;
+{
+    return [UIColor colorWithRed:0.86f green:0.87f blue:0.87f alpha:1.0f];
+}
 
 @end
